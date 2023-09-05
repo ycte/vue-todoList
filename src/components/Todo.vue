32aada2ce2de22e6577393a3e40b91ec2b0e18af
ycte <114587040+ycte@users.noreply.github.com>
2023-09-05 16:50:18 +0800

    beta:test right input

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,15 +1,28 @@
 <script setup>
 import { ref, computed } from "vue";
+import { Velocity } from "velocity-animate";
 import TodoItem from "./TodoItem.vue";
 
 const todos = ref([
   {
+    id: 0,
     title: "吃饭",
-    completed: true
-  }
+    completed: true,
+  },
 ]); // 所有备忘列表
 const newTodo = ref(""); // 输入框输入的内容
-const editedTodo = ref({}); // 当前编辑的todo
+const remaining = computed(() => {
+  return todos.value.filter((todo) => !todo.completed).length;
+}); // 未完成数量
+const pluralize = computed(() => {
+  return remaining.value === 1 || remaining.value === 0 ? "item" : "items";
+}); // 未完成计算单位
+const matchTodos = computed(() => {
+  return todos.value.filter((todo) => {
+    return todo.title.toLowerCase()
+      .indexOf(newTodo.value.toLowerCase()) > -1;
+  });
+}); // 与当前输入匹配的todo
 
 // 添加备忘
 let id = 1;
@@ -25,25 +38,37 @@ function addTodo() {
   });
   newTodo.value = "";
   console.log("添加成功", todos.value);
+  console.log("匹配结果", matchTodos.value);
 }
 
-
-
-// 未完成数量
-const remaining = computed(() => {
-  return todos.value.filter((todo) => !todo.completed).length;
-});
-// 未完成计算单位
-const pluralize = computed(() => {
-  return (remaining.value === 1 || remaining.value === 0)
-    ? "item" : "items";
-});
 // 清除已完成
 function clearCompleted() {
   todos.value = todos.value.filter((todo) => !todo.completed);
 }
 
-
+/* 动画效果 */
+// 进入中
+function beforeEnter(el) {
+  // el.style.opacity = 0;
+  // el.style.height = 0;
+}
+function enter(el, done) {
+  // 设置延时
+  var delay = el.dataset.index * 150;
+  // setTimeout(function () {
+  //   // 更新元素样式
+  //   Velocity(el, { opacity: 1, height: "58px" }, { complete: done });
+  // }, delay);
+}
+// 离开时
+function leave(el, done) {
+  // 设置延时
+  var delay = el.dataset.index * 150;
+  // setTimeout(function () {
+  //   // 更新元素样式
+  //   Velocity(el, { opacity: 0, height: 0 }, { complete: done });
+  // }, delay);
+}
 </script>
 
 <template>
@@ -56,26 +81,33 @@ function clearCompleted() {
     @keyup.enter="addTodo"
   />
 
-  <!-- 所有备忘列表 -->
-  <ul class="todo-list">
+  <!-- 添加 before-enter、enter 和 leave 的钩子 -->
+  <transition-group
+    name="staggered-fade"
+    tag="ul"
+    v-bind:css="false"
+    v-on:before-enter="beforeEnter"
+    v-on:enter="enter"
+    v-on:leave="leave"
+    class="todo-list"
+  >
+    <!-- v-for 遍历匹配中的备忘 computedTodos，key 绑定备忘 id，class 绑定样式 -->
     <li
-      v-for="todo in todos"
+      v-for="todo in matchTodos"
       class="todo"
       :key="todo.id"
-      :class="{ completed: todo.completed, editing: todo.id == editedTodo.id }"
+      :class="{ completed: todo.completed }"
     >
-      <!-- 使用 props 和 emit 进行父组件和子组件之间的通信 -->
+      <!-- <h1>{{ todo.id }}</h1> -->
       <TodoItem
         :title="todo.title"
-        @update:title="(title) => todo.title = title"
+        @update:title="(title) => (todo.title = title)"
         :completed="todo.completed"
-        @update:completed="(completed) => todo.completed = completed"
+        @update:completed="(completed) => (todo.completed = completed)"
         @delete="() => todos.value.splice(todos.value.indexOf(todo), 1)"
-        
       ></TodoItem>
-      
     </li>
-  </ul>
+  </transition-group>
 
   <!-- 相关信息与快速操作 -->
   <footer class="footer" v-show="todos.length">
